e6f194da621da3c5aba97dc3252e5bd2e28989fe
divinity76 <divinity76@gmail.com>
2022-09-10 18:05:40 +0200

    formatting

--- src/php/index.php
+++ src/php/index.php
@@ -1,28 +1,41 @@
 <?php
+
 declare(strict_types=1);
-function getRemoteIp():string{
-        // first check cloudflare ip
-        if(!empty($_SERVER['HTTP_CF_CONNECTING_IP'])){return $_SERVER['HTTP_CF_CONNECTING_IP'];}
-        // then check Fastly IP
-        if(!empty($_SERVER['HTTP_FASTLY_CLIENT_IP'])){return $_SERVER['HTTP_FASTLY_CLIENT_IP'];}
-        // netiher CF nor Fastly..
-        if(!empty($_SERVER['REMOTE_ADDR'])){return $_SERVER['REMOTE_ADDR'];}
-        throw new \LogicException("unable to find remote ip!");
+function getRemoteIp(): string
+{
+    // first check cloudflare ip
+    if (!empty($_SERVER['HTTP_CF_CONNECTING_IP'])) {
+        return $_SERVER['HTTP_CF_CONNECTING_IP'];
+    }
+    // then check Fastly IP
+    if (!empty($_SERVER['HTTP_FASTLY_CLIENT_IP'])) {
+        return $_SERVER['HTTP_FASTLY_CLIENT_IP'];
+    }
+    // netiher CF nor Fastly..
+    if (!empty($_SERVER['REMOTE_ADDR'])) {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+    throw new \LogicException("unable to find remote ip!");
 }
-function defaultHeaders(){
-        http_response_code(200);
-        header("Content-Type: text/plain; charset=utf-8");
-        // header("Content-Encoding: identity");
-        header("server: "); // << doesn't work
-        header("Cache-Control: no-cache");
-        //header("expires:");
+function defaultHeaders()
+{
+    http_response_code(200);
+    header("Content-Type: text/plain; charset=utf-8");
+    // header("Content-Encoding: identity");
+    header("server: "); // << doesn't work
+    header("Cache-Control: no-cache");
+    //header("expires:");
 }
 defaultHeaders();
 $ip = getRemoteIp();
 $host = (string)($_SERVER['HTTP_HOST'] ?? "");
-if(0===stripos($host, 'www.')){$host=substr($host,strlen('www.'));}
+if (0 === stripos($host, 'www.')) {
+    $host = substr($host, strlen('www.'));
+}
 $host = explode(".", $host, 2)[0];
 $str = $ip;
-if($host==="ipn"){$str.="\n";}
-header("Content-Length: ".strlen($str));
+if ($host === "ipn") {
+    $str .= "\n";
+}
+header("Content-Length: " . strlen($str));
 echo $str;
